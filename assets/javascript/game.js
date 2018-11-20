c1be392213e1c9540f2f88060a84a917e8c6e9e2
xpetrus <phsiao24@gmail.com>
2018-11-19 19:39:47 -0500

    functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,26 +9,59 @@ var magicLet = letters[Math.floor(Math.random()*letters.length)];
 
 var wins=0;
 var losses=0;
+var guesses = 12;
+var html = ""; 
+var gsf=[];
 
+function reset(){
+    guesses = 12;
+    gsf= [];
+    document.querySelector("#gLeft").innerHTML = guesses;
+    document.querySelector("#uGuess").innerHTML = gsf; 
+    magicLet = letters[Math.floor(Math.random()*letters.length)];
+}
 
 document.onkeyup=function(event){
     var input = event.key;
+    
 
     if(input.toLowerCase() === magicLet){
-        alert("You Win");
+        reset();
+        alert("You Guessed it!");
+        alert("Guesses Left Replenished.")
         wins++;
+        document.querySelector("#wins").innerHTML = wins;
+
+
     }
+    
     else{
         alert("Incorrect");
         losses++;
         console.log(magicLet);
-    }
 
+        if(guesses == 0)
+        {
+            alert("You ran out of guesses. You Lose. The letter to guess was : "+magicLet);
+            alert("Resetting...");
+            wins = 0 ;
+            document.querySelector("#wins").innerHTML = wins;
+            losses = 0;
+            document.querySelector("#losses").innerHTML = losses;
+            reset();
+        }
+        else{
+       
+        document.querySelector("#losses").innerHTML = losses;
+        guesses--;
+        document.querySelector("#gLeft").innerHTML = guesses;
 
+        gsf.push(input);    console.log(gsf); 
+        var html = gsf;
+        
+        document.querySelector("#uGuess").innerHTML = html;    }}
 
-
-    //document.querySelector("#uGuess").innerHTML = input;
-
-
+    
+        //document.querySelector("#uGuess").innerHTML = input;
 
 }
